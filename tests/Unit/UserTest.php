0a430aff66bf0f40a0303402fe9c7dc31f26cec1
Shamix <nsammi11@gmail.com>
2018-11-17 19:24:40 -0500

    Tasks: Seeding and Unit Testing Assignment

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = user::inRandomOrder()-> first();
+        $this->assertInternalType('int',$user->id);
+        $this->assertInstanceOf('App\User',$user);
+
+    }
+}
